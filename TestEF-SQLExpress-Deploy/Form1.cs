13c4e0e69c4576eb018c87b1074aeb435278c68f
sheri <sheri@CANTOR>
2017-02-01 09:05:02 -0800

    Add project files.

---
+++ TestEF-SQLExpress-Deploy/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestEF_SQLExpress_Deploy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
