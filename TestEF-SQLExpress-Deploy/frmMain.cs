af2475e3847531f656ce1aff6543c750ca31bda4
sheri <sheri@CANTOR>
2017-02-08 17:11:10 -0800

    ok full load/edit/save/deploy cycle works. next step: create & populate the db at install time.

--- TestEF-SQLExpress-Deploy/frmMain.cs
+++ TestEF-SQLExpress-Deploy/frmMain.cs
@@ -28,35 +28,68 @@ namespace TestEF_SQLExpress_Deploy
             MessageBox.Show("sup!");
         }
 
-        private void frmMain_Load(object sender, EventArgs e)
+        private void LoadListbox()
         {
             // load up the pkid listbox
-
+            lbIDs.Items.Clear();
             using (TestEFDeployEntities ctx = new TestEFDeployEntities())
             {
                 var mypkids = (from rows in ctx.TestTables
-                              select rows).ToList();
+                               select rows).ToList();
 
                 foreach (var row in mypkids)
                 {
                     lbIDs.Items.Add(row.pkid.ToString());
                 }
-
-                //list.Items.add(new ListBoxItem("name", "value"));
-
-                // ok what type do i foreach my way thru for the results?
-
             }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            LoadListbox();
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             // save changes to the data field to the currently selected pkid record
+            int param = GetSelectedPKID();
+
+            using (TestEFDeployEntities ctx = new TestEFDeployEntities())
+            {
+                TestTable savethis = (from rows in ctx.TestTables
+                                where rows.pkid == param
+                                select rows).FirstOrDefault();
+
+                savethis.DataValue = txtData.Text;
+
+                ctx.SaveChanges();
+            }
+        }
+
+        private int GetSelectedPKID()
+        {
+            int ret = 0;
+
+            var theone = lbIDs.SelectedItem;
+            //MessageBox.Show("selected: " + theone.ToString());
+            ret = Int32.Parse(theone.ToString());
+
+            return ret;
         }
 
         private void lbIDs_SelectedIndexChanged(object sender, EventArgs e)
         {
+            int param = GetSelectedPKID();
+
             // update data textbox with change in selected pkid
+            using (TestEFDeployEntities ctx = new TestEFDeployEntities())
+            {
+                var mypkids = (from rows in ctx.TestTables
+                               where rows.pkid == param
+                               select rows).ToList();
+
+                txtData.Text = mypkids[0].DataValue;
+            }
         }
     }
 }
