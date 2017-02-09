b8001d00122e8ad49035de51fa93f2257cdc7d97
sheri <sheri@CANTOR>
2017-02-08 16:29:51 -0800

    have an installer, have ef db connect

--- TestEF-SQLExpress-Deploy/frmMain.Designer.cs
+++ TestEF-SQLExpress-Deploy/frmMain.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.btnGo = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.lbIDs = new System.Windows.Forms.ListBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(71, 111);
+            this.btnGo.Location = new System.Drawing.Point(12, 146);
             this.btnGo.Name = "btnGo";
             this.btnGo.Size = new System.Drawing.Size(75, 23);
             this.btnGo.TabIndex = 0;
@@ -44,7 +49,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(71, 209);
+            this.btnExit.Location = new System.Drawing.Point(12, 331);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 1;
@@ -52,17 +57,68 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // lbIDs
+            // 
+            this.lbIDs.FormattingEnabled = true;
+            this.lbIDs.Location = new System.Drawing.Point(179, 34);
+            this.lbIDs.Name = "lbIDs";
+            this.lbIDs.Size = new System.Drawing.Size(120, 238);
+            this.lbIDs.TabIndex = 2;
+            this.lbIDs.SelectedIndexChanged += new System.EventHandler(this.lbIDs_SelectedIndexChanged);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(179, 296);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(120, 20);
+            this.txtData.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PKID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Data:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(200, 331);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(221, 261);
+            this.ClientSize = new System.Drawing.Size(352, 366);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.lbIDs);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnGo);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Test";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +126,11 @@
 
         private System.Windows.Forms.Button btnGo;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lbIDs;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- TestEF-SQLExpress-Deploy/frmMain.cs
+++ TestEF-SQLExpress-Deploy/frmMain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,37 @@ namespace TestEF_SQLExpress_Deploy
         private void btnGo_Click(object sender, EventArgs e)
         {
             MessageBox.Show("sup!");
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // load up the pkid listbox
+
+            using (TestEFDeployEntities ctx = new TestEFDeployEntities())
+            {
+                var mypkids = (from rows in ctx.TestTables
+                              select rows).ToList();
+
+                foreach (var row in mypkids)
+                {
+                    lbIDs.Items.Add(row.pkid.ToString());
+                }
+
+                //list.Items.add(new ListBoxItem("name", "value"));
+
+                // ok what type do i foreach my way thru for the results?
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // save changes to the data field to the currently selected pkid record
+        }
+
+        private void lbIDs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // update data textbox with change in selected pkid
         }
     }
 }
